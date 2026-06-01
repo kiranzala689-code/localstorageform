799d7ce59e93429e673ce06ae6cd4c813f345ca3
kiranzala689-code <kiranzala689@gmail.com>
2026-06-01 15:02:49 +0530

    Added local storage form project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Form from './Comp/Form'
+import Form2 from './Comp/Form2'
+import Localstorageform from './Comp/Localstorageform'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Form/> */}
+{/* <Form2/> */}
+<Localstorageform/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
